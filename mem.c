b42a4cc29926b0438d85a8c8b14560259b80ed3d
NickSettler <moiseevnikita14@gmail.com>
2022-05-02 21:26:27 +0200

    updates shared memory filling and destroying

--- mem.c
+++ mem.c
@@ -10,7 +10,7 @@
 mem_t *mem_init() {
     mem_t *mem = malloc(sizeof(mem_t));
 
-    key_t key = ftok("xmoise01/shm", 65);
+    key_t key = ftok("main.c", 'c');
 
     int shmid = shmget(key, sizeof(mem_t), 0666 | IPC_CREAT);
 
@@ -27,26 +27,24 @@ mem_t *mem_init() {
 }
 
 void mem_fill(mem_t *mem, int NO, int NH, int TI, int TB) {
-    mem->line = 0;
-    mem->o_count = NO;
-    mem->h_count = NH;
-    mem->current_m = 0;
-    mem->m_id = 0;
-    mem->current_m_h = 0;
-    mem->atom_time = TI;
-    mem->molecule_time = TB;
-    mem->current_h = 0;
-    mem->current_o = 0;
+//    mem->line = 0;
+//    mem->current_m = 0;
+//    mem->m_id = 0;
+//    mem->current_m_h = 0;
+//    mem->current_h = 0;
+//    mem->current_o = 0;
     mem->output_file = open_output();
 }
 
 void mem_destroy(mem_t *mem) {
     close_output(mem->output_file);
 
-    int unmap_result = shmdt(mem->mem_addr);
-    if (unmap_result == -1) ERROR
+    key_t key = mem->mem_id;
 
-    int control_result = shmctl(mem->mem_id, IPC_RMID, NULL);
+    int control_result = shmctl(key, IPC_RMID, NULL);
     if (control_result == -1) ERROR
+
+    shmdt(mem);
+
     free(mem);
 }
