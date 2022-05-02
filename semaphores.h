05ac6fd0d72ae75e7be9fca31a3e37dedc418d51
NickSettler <moiseevnikita14@gmail.com>
2022-05-02 22:31:57 +0200

    updates semaphore names

--- semaphores.h
+++ semaphores.h
@@ -6,12 +6,12 @@
 
 #define SEM_INIT(k, v)(sem_open(k, O_CREAT | O_EXCL, 0666, v))
 
-#define OUTPUT_SEM "output_sem"
-#define H_SEM "h_sem"
-#define O_SEM "o_sem"
-#define READY_SEM "ready_sem"
-#define CREATING_SEM "creating_sem"
-#define STOP_EXTRA_SEM "stop_extra_sem"
+#define OUTPUT_SEM "/output_sem"
+#define H_SEM "/h_sem"
+#define O_SEM "/o_sem"
+#define READY_SEM "/ready_sem"
+#define CREATING_SEM "/creating_sem"
+#define STOP_EXTRA_SEM "/stop_extra_sem"
 
 /**
  * Initializes semaphores
