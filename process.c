fd057605eeab81499bb3daa0661f7b4720a0e1af
NickSettler <moiseevnikita14@gmail.com>
2022-05-02 21:29:43 +0200

    removes useless process functions

--- process.c
+++ process.c
@@ -1,48 +1,5 @@
-#include <stdlib.h>
-#include <printf.h>
 #include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <semaphore.h>
-#include <stdio.h>
 #include "process.h"
-#include "helpers.h"
-
-void sub_process_oxygen(mem_t *mem, unsigned int atom_id) {
-    int wait_result = sem_wait(mem->semaphores->output_sem);
-    if (wait_result == -1) {
-        ERROR
-        exit(EXIT_FAILURE);
-    }
-
-    mem->line += 1;
-    printf("%d: %c %d: started\n", mem->line, OXYGEN, atom_id);
-    sem_post(mem->semaphores->output_sem);
-
-    usleep(RAND_INT(mem->atom_time));
-
-    exit(EXIT_SUCCESS);
-}
-
-void main_process(mem_t *mem) {
-    for (unsigned int i = 0; i < mem->o_count; i++) {
-        pid_t oxygen_pid = fork();
-        if (oxygen_pid < 0) {
-            ERROR
-        } else if (oxygen_pid == 0) {
-            sub_process_oxygen(mem, i);
-        }
-    }
-
-    for (long i = 0; i < mem->h_count; i++) {
-        pid_t hydrogen_pid = fork();
-        if (hydrogen_pid < 0) {
-            ERROR
-        } else if (hydrogen_pid == 0) {
-
-        }
-    }
-}
 
 process_t fork_process(unsigned int count, ATOM_TYPE type) {
     for (unsigned int i = 1; i <= count; i++) {
--- process.h
+++ process.h
@@ -1,19 +1,8 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
-#include "mem.h"
 #include "atom.h"
 
-typedef struct process_cfg_t {
-    mem_t *mem;
-    atom_t *atom;
-} process_cfg_t;
-
-
-void sub_process_oxygen(mem_t *mem, unsigned int atom_id);
-
 process_t fork_process(unsigned int count, ATOM_TYPE type);
-
-void main_process(mem_t *mem);
 
 #endif //PROCESS_H
